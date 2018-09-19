a5b4b07b6e9e3bc4c61c006c1d32857755f4b4fb
fanhl <fanhonglin@klicen.com>
2018-09-19 10:41:18 +0800

    动态壁纸服务

---
+++ library/src/main/java/com/fanhl/wallmoving/services/ActiveWallpaperService.kt
@@ -0,0 +1,13 @@
+package com.fanhl.wallmoving.services
+
+import android.service.wallpaper.WallpaperService
+
+/**
+ * 动态壁纸服务
+ */
+class ActiveWallpaperService : WallpaperService() {
+    override fun onCreateEngine(): Engine {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
